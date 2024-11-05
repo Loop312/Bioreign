24895d1453b7f9efa903741ca5175fac2c1b0af4
Lo_op <shaikhfarhaan312@gmail.com>
2024-11-04 20:03:42 -0500

    made a basic pregame menu and changed the "button key listener" to a box

--- composeApp/src/commonMain/kotlin/org/example/bioreign/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/bioreign/App.kt
@@ -2,6 +2,7 @@ package org.example.bioreign
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -9,6 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.key.*
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
@@ -16,9 +19,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import bioreign.composeapp.generated.resources.*
 import characters.Player
 import org.example.bioreign.maps.Map
-import menus.EditKeysMenu
-import menus.InGameMenu
-import menus.PregameMenu
+import menus.*
 
 
 val player = Player()
@@ -80,17 +81,21 @@ fun game(){
 @Composable
 @Preview
 fun theApp() {
-    pregameMenu.open()
-    editKeysMenu.open()
-    keyListener.listen()
-    if (gameLoop.isPlaying) {
-        game()
-        gameLoop.GameScreen()
-        gameLoop.playerStuff()
-        map.checkCollisions()
-    }
-    Text(keyListener.pressedKeys.toString())
+    val focusRequester = remember {FocusRequester()}
     //there's an issue with the focus after clicking the "Start Game"
-    //button in PregameMenu, this button is a temporary workaround
-    Button(onClick = {},Modifier.onKeyEvent(keyListener.listener)){}
+    //button in PregameMenu, this box is a temporary workaround
+    Box(Modifier.fillMaxSize().focusRequester(focusRequester).clickable{focusRequester.requestFocus()}.onKeyEvent(keyListener.listener)) {
+        pregameMenu.open()
+        editKeysMenu.open()
+        keyListener.listen()
+        if (gameLoop.isPlaying) {
+            game()
+            gameLoop.GameScreen()
+            gameLoop.playerStuff()
+            map.checkCollisions()
+        }
+        Text(keyListener.pressedKeys.toString())
+        //not really needed anymore
+        //Button(onClick = {}, Modifier.onKeyEvent(keyListener.listener)) {}
+    }
 }
